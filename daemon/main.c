02546f651a8456c429c4ccf3067d121c5793500e
Kirk Roerig <mr.possoms@gmail.com>
2022-11-28 07:45:31 -0700

    Starting to implement daemon to record data

---
+++ daemon/main.c
@@ -0,0 +1,18 @@
+typedef struct {
+	
+} state;
+
+
+int socket_init(unsigned short port)
+{
+	int sock = socket(AF_INET, SOCK_DGRAM, 0);
+	
+	bind(sock
+
+	return -1;
+}
+
+int main(int argc, const char* argv[])
+{
+	return 0;
+}
